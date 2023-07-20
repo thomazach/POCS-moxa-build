081478fe780054e04c0b53591d4c6c8f3511f467
thomazach <86134403+thomazach@users.noreply.github.com>
2023-07-20 13:11:28 -0700

    Change of directory name

--- cameras/camera_control.py
+++ cameras/camera_control.py
@@ -35,64 +35,54 @@ def get_camera_paths_dummy():
 def get_camera_paths():
     print("Finding cameras using gphoto2...")
     out = subprocess.run(["gphoto2", "--auto-detect"], stdout=subprocess.PIPE)
-    cameraPaths = out.stdout.decode('utf-8')
+    out = out.stdout.decode('utf-8')
+    out = out.translate({ord(c): None for c in string.whitespace})
+    out = out.split("CanonEOS100D", -1)
 
-    for charsToRemove in ['Model', 'Port', 'n', '\n', ' ']:
-        cameraPaths = cameraPaths.replace(charsToRemove, '')
-
-    cameraPaths = cameraPaths.split("CanonEOS100D")
-
-    primary_camera_path, secondary_camera_path = cameraPaths
+    primary_camera_path = out[1]
+    secondary_camera_path = out[2]
 
     return primary_camera_path, secondary_camera_path
 
-def take_observation(cameraSettings, iso=1):
-    cam_type, camera_path, num_captures, exposure_time, observation_dir, directoryPath = cameraSettings
-    
+def take_observation(cam_type, camera_path, num_captures, exposure_time, observation_dir, iso=1):
     if num_captures <= 0:
-        raise Exception("Bad observation data: num_captures must be a positive non-zero integer")
+        raise Exception("Bad observation data: num_captures must be positive")
 
     while True:
-        # UNCOMMENT ALL subprocess.run LINES IN PRODUCTION
         if num_captures == 0:
             break
+
+        # UNCOMMENT IN PRODUCTION
+        #os.sys(f"gphoto2 --port {camera_path} --set-config iso={iso} --filename /home/uname/moxa-pocs/images/{observation_dir}/{cam_type}/astro_image_{num_captures}.cr2 --set-config-index shutterpseed=0 --wait-event=1s --set-config-index eosremoterelease=2 --wait-event={exposure_time}s --set-config-index eosremoterelease=4 --wait-event-and-download=2s")
         
+        time.sleep(exposure_time) # May or may not be necessary, don't have tesing setup yet
+
+        # TEST PRINT SINCE ON WINDOWS
+        print(f"gphoto2 --port {camera_path} --set-config iso={iso} --filename /home/uname/moxa-pocs/images/{observation_dir}/{cam_type}/astro_image_{num_captures}.cr2 --set-config-index shutterpseed=0 --wait-event=1s --set-config-index eosremoterelease=2 --wait-event={exposure_time}s --set-config-index eosremoterelease=4 --wait-event-and-download=2s")
+
+        # UNCOMMENT IN PRODUCTION
         # Clear the camera's RAM in a hacky way to allow for back to back large exposures (tested on 120s)
-        cmdClearRAM = f"gphoto2 --port {camera_path} --set-config imageformat=0".split(' ')
-        #subprocess.run(cmdClearRAM)
-        cmdClearRAM = f"gphoto2 --port {camera_path} --set-config imageformat=9".split(' ')
-        #subprocess.run(cmdClearRAM)
-        
-        cmdArgs = f"gphoto2 --port {camera_path} --set-config iso={iso} --filename {directoryPath}/{observation_dir}/{cam_type}/astro_image_{num_captures}.cr2 --set-config-index shutterpseed=0 --wait-event=1s --set-config-index eosremoterelease=2 --wait-event={exposure_time}s --set-config-index eosremoterelease=4 --wait-event-and-download=2s".split(' ')
-        #subprocess.run(cmdArgs)
-
-        # REMOVE IN PRODUCTION, TEST PRINT SINCE ON WINDOWS
-        print(cmdArgs)
-
+        #os.sys(f"gphoto2 --port {camera_path} --set-config imageformat=0")
+        #os.sys(f"gphoto2 --port {camera_path} --set-config imageformat=9")
         num_captures -= 1
 
 def initialize_observation(cam_observation_dict):
-    # UNCOMMENT ALL subprocess.run LINES IN PRODUCTION
 
     format = "%Y-%m-%dT%H:%M:%S"
     timezone = datetime.timezone.utc
     time_and_date = datetime.datetime.now(tz=timezone).strftime(format)
 
-    directoryPath=os.path.dirname(os.path.abspath(__file__)).replace('cameras', 'images')
-    cmdMakeObservationDirectory = f"cd /home/uname/moxa-pocs/images; mkdir {time_and_date}".split(' ')
-    #subprocess.run(cmdMakeObservationDirectory)
+    #os.sys(f'cd /home/uname/moxa-pocs/images; mkdir {time_and_date}' )
 
     primary_camera_path, secondary_camera_path = get_camera_paths_dummy() # Replace with get_camera_paths in production
     primary_camera = ('Primary_Cam', primary_camera_path)
     secondary_camera = ('Secondary_Cam', secondary_camera_path)
 
-    cameraSettings = [primary_camera[0], primary_camera[1], cam_observation_dict['primary_cam']['num_captures'], cam_observation_dict['primary_cam']['exposure_time'], time_and_date, directoryPath]
-
     if cam_observation_dict['primary_cam']['take_images']:
-        threading.Thread(target=take_observation, args=([cameraSettings])).start()
+        threading.Thread(target=take_observation, args=(primary_camera[0], primary_camera[1], cam_observation_dict['primary_cam']['num_captures'], cam_observation_dict['primary_cam']['exposure_time'], time_and_date)).start()
 
     if cam_observation_dict['secondary_cam']['take_images']:
-        take_observation(cameraSettings)
+        take_observation(secondary_camera[0], secondary_camera[1], cam_observation_dict['secondary_cam']['num_captures'], cam_observation_dict['secondary_cam']['exposure_time'], time_and_date)
 
 config = give_dummy_command_data()
 
