99eae489bcd2b0555e1739396b95492c1e2df510
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 15:47:51 -0700

    start-end marker reading of serial

--- arduino/power_board/power_board.ino
+++ arduino/power_board/power_board.ino
@@ -1,5 +1,9 @@
-int reboot = 0;
-int cmd;
+const byte maxChars = 32;
+char recievedChars[maxChars];
+
+bool cmd_recieved = false;
+
+
 void setup(){
     Serial.begin(9600);
     Serial.setTimeout(5);
@@ -7,23 +11,43 @@ void setup(){
 }
 
 void loop() {
+  recieveCommand();
+  if (cmd_recieved == true) {
+    Serial.println(recievedChars);
+    cmd_recieved = false;
+  }
 
-  while (Serial.available() > 0){
-    cmd = Serial.readStringUntil(',').toInt();
-  }
-  Serial.println(cmd);
-/*
-  switch (cmd){
-    case 'heartbeat':
-      reboot = 0;
-      continue
-    case '':
-      if (reboot > 1800){
-        Serial.println("Time out reached!")
-        // toggle main processors power
-      }
-      reboot += 1;
-  }
-  */
   delay(100);
+}
+
+void recieveCommand() {
+  static bool inProgress = false;
+  static byte i = 0;
+  char startChar = '<';
+  char endChar = '>';
+  char incomingChar;
+
+  while (Serial.available() && cmd_recieved == false) {
+    incomingChar = Serial.read();
+
+    if (incomingChar == startChar) {
+      inProgress = true;
+    }
+    else if (inProgress == true) {
+      if (incomingChar != endChar) {
+        recievedChars[i] = incomingChar;
+        i++;
+        if (i >= maxChars) {
+          i = maxChars - 1;
+        }
+      }
+      else {
+        recievedChars[i] = '\0';
+        inProgress = false;
+        i = 0;
+        cmd_recieved = true;
+      }
+    }
+  }
+
 }
