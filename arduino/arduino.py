586054992375dca7dffe701d0f1201059eaa5030
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 14:08:31 -0700

    Initial arduino infrastructure, going to test on dev unit

---
+++ arduino/arduino.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
+
+def getTestSerialCommands():
+    pin_num = input("Enter pin number:")
+    state = input("Enter on or off:")
+    value = f"{pin_num},{state}"
+    return bytes(value)
+
+while True:
+
+    cmd = getTestSerialCommands()
+
+    with serial.Serial(DEFAULT_ARDUINO_PORT, 9600) as arduinoPort:
+        arduinoPort.write(cmd)
+        time.sleep(3)
+        output = arduinoPort.readline()
+        print(f"From arduino: {output}")
+
---
+++ arduino/power_board/power_board.ino
@@ -0,0 +1,26 @@
+int reboot = 0;
+String cmd;
+void setup(){
+    Serial.begin(9600);
+    Serial.setTimeout(1);
+}
+
+void loop() {
+
+  cmd = Serial.readString();
+  Serial.print(cmd);
+/*
+  switch (cmd){
+    case 'heartbeat':
+      reboot = 0;
+      continue
+    case '':
+      if (reboot > 1800){
+        Serial.println("Time out reached!")
+        // toggle main processors power
+      }
+      reboot += 1;
+  }
+  */
+  delay(1000);
+}
