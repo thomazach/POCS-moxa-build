bf22fa1d33d6679b0b3d465ee9042a59a24da18d
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 16:09:32 -0700

    Testing 5

--- arduino/power_board/power_board.ino
+++ arduino/power_board/power_board.ino
@@ -1,9 +1,3 @@
-const byte maxChars = 32;
-char recievedChars[maxChars];
-
-bool cmd_recieved = false;
-
-
 void setup(){
     Serial.begin(9600);
     Serial.setTimeout(5);
@@ -11,43 +5,6 @@ void setup(){
 }
 
 void loop() {
-  recieveCommand();
-  if (cmd_recieved == true) {
-    Serial.println(recievedChars);
-    cmd_recieved = false;
+  Serial.println("Test message!")
   }
-}
-
-void recieveCommand() {
-  static bool inProgress = false;
-  static byte i = 0;
-  char startChar = '<';
-  char endChar = '>';
-  char incomingChar;
-
-  while (Serial.available() && cmd_recieved == false) {
-    incomingChar = Serial.read();
-    Serial.println(incomingChar);
-
-
-    if (incomingChar == startChar) {
-      inProgress = true;
-    }
-    else if (inProgress == true) {
-      if (incomingChar != endChar) {
-        recievedChars[i] = incomingChar;
-        i++;
-        if (i >= maxChars) {
-          i = maxChars - 1;
-        }
-      }
-      else {
-        recievedChars[i] = '\0';
-        inProgress = false;
-        i = 0;
-        cmd_recieved = true;
-      }
-    }
-  }
-
 }
