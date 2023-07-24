e081ef2ce09e4adc5c2daf5de8699397ee95d970
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 14:47:33 -0700

    More testing...

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -53,21 +53,20 @@ with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
 
 DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
 
+'''
 def getTestSerialCommands():
     pin_num = input("Enter pin number:")
     state = input("Enter on or off:")
     value = f"{pin_num},{state}".encode("utf-8")
     return bytes(value)
+'''
+with serial.Serial(DEFAULT_ARDUINO_PORT, 9600) as arduinoPort:
+    while True:
 
-while True:
+        #cmd = getTestSerialCommands()
 
-    cmd = getTestSerialCommands()
-
-    with serial.Serial(DEFAULT_ARDUINO_PORT, 9600) as arduinoPort:
-        arduinoPort.write(cmd)
-        time.sleep(3)
-        output = arduinoPort.readline()
-        print(f"From arduino: {output}")
+            output = arduinoPort.readline()
+            print(f"From arduino: {output}")
 
 <<<<<<< HEAD
 >>>>>>> 073a191 (Initial arduino infrastructure, going to test on dev unit)
--- arduino/power_board/power_board.ino
+++ arduino/power_board/power_board.ino
@@ -1,6 +1,7 @@
 
-  cmd = Serial.readString();
-  Serial.println(cmd);
+  //cmd = Serial.readString();
+  //Serial.println(cmd);
+  Serial.println("Example data from arduino :)")
 /*
   switch (cmd){
     case 'heartbeat':
