2736598beccb73048ed6497fd29d423eaad657a6
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 15:47:51 -0700

    start-end marker reading of serial

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -13,7 +13,7 @@ def getTestSerialCommands():
 cmd = getTestSerialCommands()
 '''
 
-cmd = '5,'
+cmd = '<1, 0>'
 with serial.Serial(DEFAULT_ARDUINO_PORT, 9600) as arduinoPort:
     arduinoPort.write(bytes(cmd, 'utf-8'))
     while True:
