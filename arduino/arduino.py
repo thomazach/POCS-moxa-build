65332f81342a867b9e602fa1b1284c4fef66a5ef
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 19:32:19 -0700

    Testing 19

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -56,14 +56,13 @@ DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
 
 def getTestSerialCommands():
     command = input(">>")
-    value = bytes(f'<{command}>', 'utf-8')
+    value = f'<{command}>'
     return value
 
-# 
 cmd = getTestSerialCommands()
 
 with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
-    arduinoPort.write(cmd)
+    arduinoPort.write(bytes(cmd, "utf-8"))
     while True:
             output = arduinoPort.readline()
             print(output)
