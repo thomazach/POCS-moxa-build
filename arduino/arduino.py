6fa030622e5b4bf8f686a07cb1113f375b27f6a5
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 19:21:03 -0700

    testing 18

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -3,17 +3,15 @@ import time
 
 DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
 
-'''
+
 def getTestSerialCommands():
-    pin_num = input("Enter pin number:")
-    state = input("Enter on or off:")
-    value = f"{pin_num},{state}"
+    command = input(">>")
+    value = bytes(f'<{command}>', 'utf-8')
     return value
 
+# 
 cmd = getTestSerialCommands()
-'''
 
-cmd = b'<'
 with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
     arduinoPort.write(cmd)
     while True:
