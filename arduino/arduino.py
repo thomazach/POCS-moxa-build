b2697da511a4910705f7c24facd09e99712c1061
Zach Thomas <thomazac@oregonstate.edu>
2023-07-24 18:09:32 -0700

    Testing 13

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -17,6 +17,7 @@ cmd = b'<off, 1><give_temps>'
 with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=1, rtscts=True) as arduinoPort:
     arduinoPort.write(cmd)
     while True:
+            time.sleep(1)
             output = arduinoPort.readline().decode("utf-8")
             arduinoPort.write(cmd)
             print(output)
