a79c949d7228757298ea7d91c7cda7f7d8d5fd31
Zach Thomas <thomazac@oregonstate.edu>
2023-07-25 13:21:44 -0700

    Feature testing, command parsing and execution

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -3,6 +3,26 @@ import time
 
 DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
 
+'''
+Arduino expects:
+"<command_integer,argument_integer>"
+command integers:
+    0:
+        Turns off a relay specified by argument_integer (0 to 4)
+    1: 
+        Turns on a relay specified by argument_integer (0 to 4)
+    2:
+        Arduino measures electrical current values and sends them to serial
+        with a start and end marker of "|"
+    
+Example: <1,1> turns off relay in relayArray[1]
+RELAY_0 ---> Weather sensor
+RELAY_1 ---> Unassigned
+RELAY_2 ---> Fan
+RELAY_3 ---> Cameras
+RELAY_4 ---> Mount
+'''
+
 
 def getTestSerialCommands():
     command = input(">>")
@@ -10,7 +30,6 @@ def getTestSerialCommands():
     return value.encode("utf-8")
 
 cmd = getTestSerialCommands()
-print(cmd)
 
 with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
     time.sleep(5)
@@ -18,3 +37,7 @@ with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
     while True:
             output = arduinoPort.readline().decode('utf-8')
             print(output)
+            cmd = getTestSerialCommands()
+            arduinoPort.write(cmd)
+            time.sleep(2)
+
