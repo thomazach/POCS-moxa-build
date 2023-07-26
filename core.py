87e07bc8706fe3d0d75f31dde5dff193ee2a0d69
Lord-Topa <topavelal@gmail.com>
2023-07-25 19:41:25 -0700

    made core send the target as a pickle object

--- core.py
+++ core.py
@@ -38,12 +38,8 @@ def main():
             target_queue = obs_scheduler.getTargetQueue(TARGETS_FILE_PATH)
             while target_queue != []:
                 target = heapq.heappop(target_queue)
-                if not checkTargetAvailability(target.position):
-                    continue
-                # tell mount controller target
-                with open("pickle/current_target.pickle", "wb") as pickleFile:
-                    pickle.dump(target, pickleFile)
-                os.system('python mount/mount_control.py')
+                with open('pickle/current_target.pickle', 'wb') as pipe:
+                    pickle.dump(target, pipe)
                 # wait for mount to say complete
                 while True:
                     time.sleep(30)
