498236c8818133267a55bcffe05b6afd7b352b7a
Lord-Topa <topavelal@gmail.com>
2023-08-03 18:56:26 -0700

    Started working on CLI functionality

--- core.py
+++ core.py
@@ -1,22 +1,68 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
 import sys
 import os
 import threading
 import heapq
 import time
-import random
 import pickle
+import subprocess
 from observational_scheduler import obs_scheduler
 
 WEATHER_RESULTS_TXT = 'weather_results.txt'
 TARGETS_FILE_PATH = 'conf_files/test_fields.yaml'
-random.seed(time.time_ns)
 
 yesOrNo = lambda x: x == 'y' or x == 'Y' or x == 'yes' or x == 'Yes'
+
+class command:
+    def __init__(self, userInput):
+        try:
+            parts = userInput.split()
+        except Exception as error:
+            print('=ERROR= ', error)
+
+        if len(parts) == 1:
+            cmd = parts[0]
+            realFile = None
+            for file in os.listdir('./user_scripts'):
+                if file.split('.')[0] == cmd:
+                    realFile = file
+            subprocess.run('./user_scripts/' + realFile)
+        elif parts[0] == '&':
+            print('background process not in yet')
+        else:
+            cmd = parts[0]
+            args = parts[1:]
+            realFile = None
+            for file in os.listdir('./user_scripts'):
+                if file.split('.')[0] == cmd:
+                    realFile = file
+            commandArray = ['./user_scripts/' + realFile]
+            subprocess.Popen(commandArray + args, cwd=os.path.dirname(os.path.realpath(__file__)))
+            print('annoying')
 
 def _writeToFile(PATH, msg):
     file_write = open(PATH, "w")
     file_write.write(msg)
     file_write.close()
+
+def _betterInput(prompt, Type = str, default = None):
+    #TODO: Implement the mountCommand class so that I can also have it 
+    #      handled here
+
+    result = None
+    while result == None:
+        userInput = input(prompt) or default
+        if userInput:
+            try:
+                result = Type(userInput)
+            except TypeError as error:
+                print('=INVALID= Your input was not of type ', Type)
+                userInput = None
+            except Exception as error:
+                print('=ERROR= ', error)
+                userInput = None
+    return result
 
 def makeObservationDict():
 
@@ -28,15 +74,15 @@ def makeObservationDict():
         camera['exposure_time'] = None
         camera['take_images'] = yesOrNo(input('Do you want this camera to take images [y/n]: '))
         if camera['take_images']:
-            camera['num_captures'] = int(input('Enter # of images to capture: ') or 1)
-            camera['exposure_time'] = int(input('Enter exposure time per image in seconds: ') or 1)
+            camera['num_captures'] = _betterInput('Enter # of images to capture: ', 1)
+            camera['exposure_time'] = _betterInput('Enter exposure time per image in seconds: ', 1)
         return camera
 
-    note = str(input('Enter user note [leave blank if none]: ') or None)
-    priority = int(input('Input the priority of the observation \nas a positive whole number: ') or 0)
-    ra = str(input('Input the ra: ') or '00 42 44')
-    dec = str(input('Input the dec: ') or '+41 16 09')
-    cmd = str(input('Input the command [leave blank for slew]: ') or 'slew to target')
+    note = _betterInput('Enter user note [leave blank if none]: ', default='None')
+    priority = _betterInput('Input the priority of the observation as a positive whole number: ', 0)
+    ra = _betterInput('Input the ra: ', default='00 42 44')
+    dec = _betterInput('Input the dec: ', default='+41 16 09')
+    cmd = _betterInput('Input the command [leave blank for slew]: ', default='slew to target')
     print('Primary Camera Settings: \n')
     primaryCam = _makeCameraArr()
     print('Secondary Camera Settings: \n')
@@ -51,7 +97,6 @@ def makeObservationDict():
     attributes['secondary_cam'] = secondaryCam
     return attributes
 
-
 def readWeatherResults(PATH):
     weather_results_file_object = open(PATH, 'r')
     weather_results = weather_results_file_object.readline()
@@ -64,56 +109,59 @@ def checkTargetAvailability(target):
 
 def main():
 
-    keepGettingObservations = True
-    observationsDict = {}
-    while keepGettingObservations:
-        name = str(input('Name of the observation: '))
-        attributes = makeObservationDict()
-        observationsDict[name] = attributes
-        keepGettingObservations = yesOrNo(input('Add another observation [y/n]: '))
-    obs_scheduler.createObservationList(observationsDict)
+    test1 = _betterInput("Command: ", command, None)
+    print('Mtest1: ', test1, ' \nMtype(test1): ', str(type(test1)))
 
-    _writeToFile(WEATHER_RESULTS_TXT, 'go')
-    _writeToFile(WEATHER_RESULTS_TXT, 'true') # Temporarily need to bypass weather module until panoptes team figures out solution for weather sensor
+    # keepGettingObservations = True
+    # observationsDict = {}
+    # while keepGettingObservations:
+    #     name = str(input('Name of the observation: '))
+    #     attributes = makeObservationDict()
+    #     observationsDict[name] = attributes
+    #     keepGettingObservations = yesOrNo(input('Add another observation [y/n]: '))
+    # obs_scheduler.createObservationList(observationsDict)
 
-    while True: 
+    # _writeToFile(WEATHER_RESULTS_TXT, 'go')
+    # _writeToFile(WEATHER_RESULTS_TXT, 'true') # Temporarily need to bypass weather module until panoptes team figures out solution for weather sensor
+
+    # while True: 
         
-        time.sleep(3)
-        weather_results = readWeatherResults(WEATHER_RESULTS_TXT)
-        print(weather_results)
-        if weather_results == 'true':
-            print('Safe to use')
-            target_queue = obs_scheduler.getTargetQueue(TARGETS_FILE_PATH)
-            while target_queue != []:
-                target = heapq.heappop(target_queue)
-                # tell mount controller target
-                with open("pickle/current_target.pickle", "wb") as pickleFile:
-                    pickle.dump(target, pickleFile)
-                os.system('python mount/mount_control.py')
-                # wait for mount to say complete
-                while True:
-                    time.sleep(30)
-                    with open("pickle.current_target.pickle", "rb") as f:
-                        target = pickle.load(f)
+    #     time.sleep(3)
+    #     weather_results = readWeatherResults(WEATHER_RESULTS_TXT)
+    #     print(weather_results)
+    #     if weather_results == 'true':
+    #         print('Safe to use')
+    #         target_queue = obs_scheduler.getTargetQueue(TARGETS_FILE_PATH)
+    #         while target_queue != []:
+    #             target = heapq.heappop(target_queue)
+    #             # tell mount controller target
+    #             with open("pickle/current_target.pickle", "wb") as pickleFile:
+    #                 pickle.dump(target, pickleFile)
+    #             os.system('python mount/mount_control.py')
+    #             # wait for mount to say complete
+    #             while True:
+    #                 time.sleep(30)
+    #                 with open("pickle.current_target.pickle", "rb") as f:
+    #                     target = pickle.load(f)
                     
-                    # TODO: Add safety feature that sends the mount the emergency park command if this loop has ran 10+ min longer than expected observation time (could also send raw serial)
+    #                 # TODO: Add safety feature that sends the mount the emergency park command if this loop has ran 10+ min longer than expected observation time (could also send raw serial)
 
-                    if target.cmd == 'observation complete':
-                        break
+    #                 if target.cmd == 'observation complete':
+    #                     break
 
-                # get data from camera
-                # ask storage if full 
-                #   if full handle or notify somehow
-                #   else upload or store picture in storage method
+    #             # get data from camera
+    #             # ask storage if full 
+    #             #   if full handle or notify somehow
+    #             #   else upload or store picture in storage method
 
-            # when it is safe to go we need to send 
-            _writeToFile(WEATHER_RESULTS_TXT, 'exit')
-            break
-        else:
-            print('not safe')
+    #         # when it is safe to go we need to send 
+    #         _writeToFile(WEATHER_RESULTS_TXT, 'exit')
+    #         break
+    #     else:
+    #         print('not safe')
 
-            # Things aren't safe so the mount needs to be told to cry
-            # break
+    #         # Things aren't safe so the mount needs to be told to cry
+    #         # break
 
 if __name__ == "__main__":
     main()
--- observational_scheduler/obs_scheduler.py
+++ observational_scheduler/obs_scheduler.py
@@ -20,31 +20,57 @@ class target:
     def __lt__(self, other):
         return self.priority < other.priority
     
+    def __le__(self, other):
+        return self.priority <= other.priority
+    
     def __eq__(self, other):
         if self.name == other.name and self.priority == other.priority and self.position == other.position:
             return True
         return False
     
+    def __gt__(self, other):
+        return self.priority > other.priority
+    
+    def __ge__(self, other):
+        return self.priority >= other.priority
+    
     def __str__(self):
         return f'---\npriority={0 - self.priority}\nname={self.name}\nposition={self.position}\ncamera_settings={self.camera_settings}\nobservation_notes={self.observation_notes}\n---'
     
-def createObservationList(observationDict):
-    with open(DATA_FILE_DEFAULT_PATH, 'w') as file:
+def createObservationList(observationDict, PATH = DATA_FILE_DEFAULT_PATH):
+    with open(PATH, 'w') as file:
         observation = yaml.dump(observationDict, file)
 
-def getTargetQueue(PATH):
+def getTargetQueue(PATH = DATA_FILE_DEFAULT_PATH):
     pQueue = []
     heapq.heapify(pQueue)
     dataDict = 0
-    with open(DATA_FILE_DEFAULT_PATH) as file:
+    with open(PATH) as file:
         dataDict = yaml.load(file, Loader=SafeLoader)
     for key, entry in dataDict.items():
         name = key
-        obs_note = entry['user_note']
+        obsNote = entry['note']
         prio = entry['priority']
         position = {'ra': entry['ra'], 'dec' :entry['dec']}
-        camera_settings = {'primary_cam' : entry['primary_cam'], 'secondary_cam' : entry['secondary_cam']}
+        cameraSettings = {'primary_cam' : entry['primary_cam'], 'secondary_cam' : entry['secondary_cam']}
         command = entry['cmd']
 
-        heapq.heappush(pQueue, target(name, position, camera_settings, priority=prio, observation_notes=obs_note, command=command))
+        heapq.heappush(pQueue, target(name, position, cameraSettings, priority=prio, observation_notes=obsNote, command=command))
     return pQueue
+
+def getTargetList(PATH = DATA_FILE_DEFAULT_PATH):
+    result = []
+    with open(PATH) as file:
+        dataDict = yaml.load(file, Loader=SafeLoader)
+    for key, entry in dataDict.items():
+        name = key
+        obsNote = entry['note']
+        prio = entry['priority']
+        position = {'ra': entry['ra'], 'dec' :entry['dec']}
+        cameraSettings = {'primary_cam' : entry['primary_cam'], 'secondary_cam' : entry['secondary_cam']}
+        command = entry['cmd']
+
+        result.append(target(name, position, cameraSettings, priority=prio, observation_notes=obsNote, command=command))
+
+    return result
+
