fae119565515761c8b4362650a9d21fd4d4db79f
thomazac <thomazac@oregonstate.edu>
2023-08-21 10:37:20 -0700

    Slewing improvement, better print statements, cleaner usage of directories

--- mount/mount_control.py
+++ mount/mount_control.py
@@ -1,4 +1,3 @@
-# NOT FULLY HARDWARE TESTED
 import os
 import sys
 import time
@@ -9,6 +8,8 @@ from astropy.coordinates import SkyCoord, Angle
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from observational_scheduler.obs_scheduler import target
+
+parentPath = os.path.dirname(__file__).replace('/mount', '')
 
 ### Testing function, should use auto detection in the future 
 def get_mount_port():
@@ -26,45 +27,46 @@ def getCurrentSkyCoord(port):
 
 def connect_to_mount():
 
-    # Open serial communication with the mount
-    with serial.Serial(get_mount_port(), 9600, timeout=1) as mountSerialPort:
-        
-        # Verify that the mount is communicating as expected
-        mountSerialPort.write(b':MountInfo#')
-        modelNumber = mountSerialPort.read(4)
-        if modelNumber != b'0030':
-            raise Exception("Incorrect model number: Bad serial communication. Required mount for this software is the iEQ30Pro from iOptron.")
-        
-        # Set the mount to slew at max speed, which is 1400 x Sidereal
-        mountSerialPort.write(b':SR9#') 
-        
-        # Get the current RA and DEC coordinates
-        currentCoordinates = getCurrentSkyCoord(mountSerialPort)
+    mountSerialPort = serial.Serial(get_mount_port(), 9600, timeout=30, parity='N')
+    mountSerialPort.open()    
 
-        # Check that the slewing rate is its slowest value for max accuracy, and set it if it isn't
-        mountSerialPort.write(b':GSR#')
-        slewingSpeed = mountSerialPort.read(2)
-        if slewingSpeed != b'7#':
-            print("Incorrect slewing rate, attempting to set it to 256x sidereal")
-            mountSerialPort.write(b':MSR7#')
-            if mountSerialPort.read(1) == b'1':
-                print("Succesfully updated slewing rate.")
-            else:
-                raise Exception("Problem setting sidereal tracking rate to 256x sidereal")
+    # Verify that the mount is communicating as expected
+    mountSerialPort.write(b':MountInfo#')
+    modelNumber = mountSerialPort.read(4)
+    if modelNumber != b'0030':
+        raise Exception("Incorrect model number: Bad serial communication. Required mount for this software is the iEQ30Pro from iOptron.")
+    
+    # Set the mount to slew at max speed, which is 1440 x Sidereal
+    mountSerialPort.write(b':SR9#')
+    out = mountSerialPort.read(1)
+    # TODO: Add log message for out serial
+    
+    # Get the current RA and DEC coordinates
+    currentCoordinates = getCurrentSkyCoord(mountSerialPort)
+
+    # Check that the slewing rate is its slowest value for max accuracy, and set it if it isn't
+    mountSerialPort.write(b':GSR#')
+    slewingSpeed = mountSerialPort.read(2)
+    if slewingSpeed != b'7#':
+        print("Incorrect slewing rate, attempting to set it to 256x sidereal")
+        mountSerialPort.write(b':MSR7#')
+        if mountSerialPort.read(1) == b'1':
+            print("Succesfully updated slewing rate.")
+        else:
+            raise Exception("Problem setting sidereal tracking rate to 256x sidereal")
         
         return mountSerialPort, currentCoordinates
     
 ### Recieve a command from moxa-pocs/core by loading the pickle instance it has provided in the pickle directory
 def request_mount_command():
-    relative_path = os.path.dirname(__file__).replace('mount', 'pickle')
-    with open(f"{relative_path}\current_target.pickle", "rb") as f:
+    with open(f"{parentPath}/pickle/current_target.pickle", "rb") as f:
         current_target = pickle.load(f)
 
     return current_target
 
 def sendTargetObjectCommand(current_target_object, cmd):
     current_target_object.cmd = cmd
-    with open("pickle/current_target.pickle", "wb") as f:
+    with open(f"{parentPath}/pickle/current_target.pickle", "wb") as f:
         pickle.dump(current_target_object, f)
 
 def create_movement_commands(current_position, desired_position):
@@ -96,9 +98,9 @@ def create_movement_commands(current_position, desired_position):
             A float representing the amount of time to move along the specified axis in SECONDS
 
     '''
-    # Find RA and DEC difference
-    dec_diff = desired_position.dec.degree - current_position.dec.degree
-    ra_diff = desired_position.ra.degree - current_position.ra.degree
+    angular_difference = lambda angle1, angle2: (angle2 - angle1 + 180) % 360 - 180
+    dec_diff = angular_difference(current_position.dec.degree, desired_position.dec.degree)
+    ra_diff = angular_difference(current_position.ra.degree, desired_position.ra.degree)
 
     # Give each axis the right command based on the difference in axis degrees
     if dec_diff < 0:
@@ -121,9 +123,9 @@ def create_movement_commands(current_position, desired_position):
     # 1 degree / 3600 arcseconds * 15.042 arcseconds/ seconds
     # = 15.042 / 3600 degrees / second 
     # 3600 / 15.042 seconds / degree
-    # 1400 is the multiple selected by :SR9# on an iEQ30Pro
-    dec_time = 3600 / 15.042 * 1400 * dec_diff
-    ra_time = 3600 / 15.042 * 1400 * ra_diff
+    # 1440 is the multiple selected by :SR9# on an iEQ30Pro
+    dec_time = abs(dec_diff) / (15.042 * 1440 * 0.000277778)
+    ra_time = abs(ra_diff) / (15.042 * 1440 * 0.000277778)
 
     dec_tuple = (dec_cmd, dec_time)
     ra_tuple = (ra_cmd, ra_time)
@@ -143,29 +145,27 @@ def execute_movement_commands(mount_serial_port, RA_tuple, DEC_tuple):
 
     dec_start_time = time.time()
     with mount_serial_port:
-        print(f"Sending serial command: {dec_cmd} with desired execution time of {dec_time} seconds")
+        print(f"Sending serial command: {dec_cmd}. Slewing DEC axis with desired execution time of {dec_time} seconds")
         mount_serial_port.write(dec_cmd)
         while (dec_start_time + dec_time + 5) > time.time():
 
             if dec_start_time + dec_time <= time.time():
-                print(f"Sending serial command ':qD#' to stop DEC mount movement at {time.time()}")
                 mount_serial_port.write(b':qD#')
-                print(f"Actual execution time is {time.time() - dec_start_time}")
+                print(f"Sent serial command ':qD#'. Stopped DEC slewing with an execution time of {time.time() - dec_start_time} seconds")
                 break
 
         ra_start_time = time.time()
-        print(f"Sending serial command: {ra_cmd} with desired execution time of {ra_time} seconds")
+        print(f"Sending serial command: {ra_cmd}. Slewing RA axis with desired execution time of {ra_time} seconds")
         mount_serial_port.write(ra_cmd)
         while (ra_start_time + ra_time + 5) > time.time():
             
             if ra_start_time + ra_time <= time.time():
-                print(f"Sent serial command ':qR#' to stop RA mount movement at {time.time()}")
                 mount_serial_port.write(b':qR#')
-                print(f"Actual execution time is {time.time() - ra_start_time}")
+                print(f"Sent serial command ':qR#'. Stoped slewing RA axis with an execution time of {time.time() - ra_start_time} seconds")
                 break
 
-        print(f"Sending serial command: ':ST1#' to start tracking...")
         mount_serial_port.write(b':ST1#')
+        print(f"Sent serial command: ':ST1#'. Tracking target.")
 
 def main():
     mount_port, START_COORDINATES = connect_to_mount()
@@ -179,19 +179,25 @@ def main():
 
             current_target = request_mount_command()
 
+            if not mount_port.is_open():
+                mount_port.open()
+
             match current_target.cmd:
 
                 case 'slew to target':
-                    mount_port.write(b':MP0#') # Unpark mount, dont need to check if its already unparked since command has no effect if already unparked
+                    print("System attempting to slew to target...")
+                    mount_port.write(b':MP0#') # command has no effect if already unparked
                     RA_tuple, DEC_tuple = create_movement_commands(START_COORDINATES, SkyCoord(current_target.position['ra'], current_target.position['dec'], unit=(u.hourangle, u.deg)))
                     execute_movement_commands(mount_port, RA_tuple, DEC_tuple)
                     sendTargetObjectCommand(current_target, 'take images')
-                    os.system('cd .. ; python cameras/camera_control.py')
+                    os.system(f'{parentPath}/cameras/camera_control.py')
 
                 case 'park':
                     print("Parking the mount.")
                     mount_port.write(b':MP1#')
                     sendTargetObjectCommand(current_target, 'parked')
+                    time.sleep(2)
+                    mount_port.close()
                     
                 case 'emergency park':
                     print("Parking the mount and aborting observation of this target")
@@ -205,6 +211,8 @@ def main():
                 case 'observation complete':
                     print("Observation complete. Parking the mount.")
                     mount_port.write(b':MP1#')
+                    time.sleep(2)
+                    mount_port.close()
                     break
                 
                 case _:
