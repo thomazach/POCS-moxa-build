9d898860e123c26a147104fce4c7edfd62081b57
thomazac <thomazac@oregonstate.edu>
2023-09-12 15:21:19 -0700

    Added logging for the arduino module

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -29,6 +29,7 @@ parentDirectory = os.path.realpath(__file__).replace('/arduino/arduino.py', '')
 sys.path.append(parentDirectory)
 
 from user_scripts.schedule import bcolors
+from logger.astro_logger import astroLogger
 
 DEFAULT_ARDUINO_PORT = '/dev/ttyACM0'
 
@@ -39,6 +40,8 @@ def serialize_commands(readable_command: str):
     Isn't case sensitive, all inputs are converted to lower case
     Doesn't support command names that have a blank space (" ")
     '''
+
+    logger.debug("Converting human readable command to an arduino serial command.")
     readable_command = readable_command.lower()
     array_cmd = list(filter(None, readable_command.split(" ")))
 
@@ -64,15 +67,19 @@ def serialize_commands(readable_command: str):
                     cmd_arg = 3
                 case "mount":
                     cmd_arg = 4
+            logger.debug(f"Arduino serial command: {f'<{cmd},{cmd_arg}>'.encode('utf-8')}")
             return f'<{cmd},{cmd_arg}>'.encode("utf-8")
         
         case "current" | "currents" | "get_current" | "get_currents":
+            logger.debug(f"Arduino serial command: {f'<2>'.encode('utf-8')}")
             return f'<2>'.encode("utf-8")
         
         case "read_weather" | "get_weather" | "read_weather_sensor" | "get_weather_sensor":
+            logger.debug(f"Arduino serial command: {f'<3>'.encode('utf-8')}")
             return f'<3>'.encode("utf-8")
 
 def listen(port):
+    # Function listens for a response from the arduino and returns the requested data or the completion character
     output = str(port.readline().decode('utf-8'))
     output = output.replace("\r", "")
     output = output.replace("\n", "")
@@ -83,21 +90,27 @@ def listen(port):
             response = []
             for entry in csvData:
                 response.append(int(entry))
+            logger.info(f"Recieved the following data requested by the command: {response}")
             return response
         elif output == "#":
+            logger.info("Recieved successful command execution character from the Arduino.")
             return bcolors.OKGREEN + "\nExecuted Command Successfully" + bcolors.ENDC
         elif output == "r":
             print(bcolors.OKGREEN + "\nArduino setup complete. Ready for commands." + bcolors.ENDC)
+            logger.info("Succesfully connected and initialized the Arduino.")
             return True
         
     elif output.count("|") != 0 and output.count("|") != 2:
-        return bcolors.YELLOW + 'WARNING: Arduino serial communication error. Expected command response within two "|" characters.' + bcolors.ENDC
+        logger.error("Arduino serial communication error. Expected command response within two '|' characters. ")
+        return bcolors.YELLOW + 'WARNING: Arduino serial communication error or timeout. Expected command response within two "|" characters.' + bcolors.ENDC
     
     else:
+        logger.warn("Unexpected serial response. Possible Arduino serial communication timeout reached.")
         print("Unexpected serial response. Possible Arduino serial communication timeout reached.")
         return None
 
 def main():
+    logger.info(f"Attempting to connect to arduino on {DEFAULT_ARDUINO_PORT}")
     with serial.Serial(DEFAULT_ARDUINO_PORT, 9600, timeout=5) as arduinoPort:
         # Can't send serial commands before setup() has completed
         ready = False
@@ -109,8 +122,10 @@ def main():
         while On:
             with open(pickleFilePath, "rb") as f:
                 commandDict = pickle.load(f)
+                logger.debug(f"Loaded arduino_cmd.pickle and recieved command dictionary: {commandDict}")
 
             if commandDict['cmd'].lower() == "off" and commandDict['execute'] == True:
+                logger.info(f"Turning off the arduino service.")
                 commandDict['execute'] = False
                 with open(pickleFilePath, "wb") as f:
                     pickle.dump(commandDict, f)
@@ -118,6 +133,7 @@ def main():
                 continue
 
             if commandDict['execute'] == True:
+                logger.info(f"Attempting to execute arduino command: {commandDict['cmd']}")
                 arduinoReadyCommand = serialize_commands(commandDict['cmd'])
                 # TODO: When logging is added log the command sent to the arduino
                 arduinoPort.write(arduinoReadyCommand)
@@ -127,8 +143,10 @@ def main():
                 commandDict['execute'] = False
                 with open(pickleFilePath, "wb") as f:
                     pickle.dump(commandDict, f)
+                    logger.debug("Arduino's response has been written to arduino_cmd.pickle.")
 
             time.sleep(1)
 
 if __name__ == "__main__":
+    logger = astroLogger(enable_color=True)
     main()
