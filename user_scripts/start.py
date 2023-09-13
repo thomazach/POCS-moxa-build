db67020b63dcf678dc92f4f6ce429775b032b022
thomazac <thomazac@oregonstate.edu>
2023-09-13 12:27:23 -0700

    Added logging to start.py

--- user_scripts/start.py
+++ user_scripts/start.py
@@ -4,9 +4,8 @@
 
 def main(args):
 
-    PARENT_DIRECTORY = os.path.dirname(__file__).replace('/user_scripts', '')
-
     if args.force:
+        logger.debug("Forcing system_info.pickle parameters to state = 'off' and desired_state = 'off'")
         with open(f"{PARENT_DIRECTORY}/pickle/system_info.pickle", "wb") as f:
             systemInfo = {'state': 'off', 'desired_state': 'off'}
             systemInfo = pickle.dump(systemInfo, f)
@@ -16,15 +15,17 @@ def main(args):
         systemInfo = pickle.load(f)
     
     if systemInfo['state'] == 'off' and systemInfo['desired_state'] == 'off':
+        logger.info("Turing on the system.")
         systemInfo['desired_state'] = 'on'
 
         time.sleep(0.5)
         
         with open(f"{PARENT_DIRECTORY}/pickle/system_info.pickle", "wb") as f:
             pickle.dump(systemInfo, f)
-        
+        logger.debug("Wrote desired_state = 'on' to system_info.pickle.")
         time.sleep(0.5)
 
+        logger.debug("Starting core module.")
         os.system(f'python {PARENT_DIRECTORY}/core/core.py')
     elif systemInfo['state'] == 'on':
         print("System is already running!")
@@ -33,11 +34,18 @@ def main(args):
 
 if __name__ == '__main__':
     import os
+    import sys
     import argparse
     import pickle
     import time
 
     from schedule import bcolors
+
+    PARENT_DIRECTORY = os.path.dirname(__file__).replace('/user_scripts', '')
+    sys.path.append(PARENT_DIRECTORY)
+    from logger.astro_logger import astroLogger
+
+    logger = astroLogger(enable_color=True)
 
     parser = argparse.ArgumentParser(description='''
 Put the unit into an automated observation state using the current system settings 
