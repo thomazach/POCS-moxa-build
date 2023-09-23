dfb84f6d8946c1e30488aaed95dfdd8df1812bfe
thomazach <thomazac@oregonstate.edu>
2023-09-23 14:51:11 -0700

    Help CLI command lists all-non directory files in user_scripts

--- user_scripts/help.py
+++ user_scripts/help.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
 def main():
+
+    thisDir = os.path.dirname(__file__)
+
+    cmdRaw = os.popen(f'ls {thisDir}').read()
+    files = cmdRaw.split('\n')
+
     print("---------------   Available PANOPTES commands   ---------------")
-    print(bcolors.OKCYAN + "arduino" + bcolors.ENDC)
-    print(bcolors.OKCYAN + "schedule" + bcolors.ENDC)
-    print(bcolors.OKCYAN + "settings" + bcolors.ENDC)
-    print(bcolors.OKCYAN + "start" + bcolors.ENDC)
-    print(bcolors.OKCYAN + "stop" + bcolors.ENDC)
+
+    for file in files:
+        if ("." in file) and (file != "help.py"):
+            print(bcolors.OKCYAN + file[0:file.find(".")] + bcolors.ENDC)
+
     print("USAGE:")
     print('''\
 Add a "-h" argument to the end of a command to get detailed information about its arguments.
@@ -15,5 +21,9 @@ Example:
           ''')
 
 if __name__ == "__main__":
+
+    import os
+
     from schedule import bcolors
+
     main()
